c1a663d1ab98e8c0751fb3edbb0d28b3737914b7
XXXRef <h1deandseek@yandex.com>
2020-01-15 21:25:08 -0800

    1. Implemented CMakeLists.exe for CLI_UI 2. Little codebase changes

--- CLI_UI/src/main.cpp
+++ CLI_UI/src/main.cpp
@@ -13,46 +13,132 @@ CLI_UI main module
 
 #include "types.hpp"
 
-std::map<CFuzzingManager::EWorkerState, std::wstring> statesNames = {
-	{ CFuzzingManager::EWorkerState::VANILLA, L"VANILLA"},
-	{ CFuzzingManager::EWorkerState::INITIALIZED, L"INITIALIZED" },
-	{ CFuzzingManager::EWorkerState::PLAYING, L"PLAYING" },
-	{ CFuzzingManager::EWorkerState::STOPPED, L"STOPPED" },
-	{ CFuzzingManager::EWorkerState::ENDED, L"ENDED" },
-	{ CFuzzingManager::EWorkerState::DEINITIALIZED, L"DEINITIALIZED" },
-	{ CFuzzingManager::EWorkerState::INTERRUPTED, L"INTERRUPTED" }
+#if defined PLATFORM_WINDOWS
+	using MAINCFG_STRTYPE=std::string;
+	#define MAINCFG_STD_IN_OBJ std::cin
+	#define MAINCFG_STD_OUT_OBJ std::cout
+	using STRSTREAM=std::stringstream;
+	//Commands
+	#define MAINCFG_CMD_EXIT "EXIT"
+	#define MAINCFG_CMD_EXIT_LC "exit"
+	#define MAINCFG_CMD_LS "LS"
+	#define MAINCFG_CMD_LS_LC "ls"
+	#define MAINCFG_CMD_STATE "STATE"
+	#define MAINCFG_CMD_STATE_LC "state"
+	#define MAINCFG_CMD_ADD "ADD"
+	#define MAINCFG_CMD_ADD_LC "add"
+	#define MAINCFG_CMD_RM "RM"
+	#define MAINCFG_CMD_RM_LC "rm"
+	#define MAINCFG_CMD_INIT "INIT"
+	#define MAINCFG_CMD_INIT_LC "init"
+	#define MAINCFG_CMD_DEINIT "DEINIT"
+	#define MAINCFG_CMD_DEINIT_LC "deinit"
+	#define MAINCFG_CMD_PLAY "PLAY"
+	#define MAINCFG_CMD_PLAY_LC "play"
+	#define MAINCFG_CMD_STOP "STOP"
+	#define MAINCFG_CMD_STOP_LC "stop"
+	#define MAINCFG_CMD_ADDLOGGER "ADD_LOGGER"
+	#define MAINCFG_CMD_ADDLOGGER_LC "add_logger"
+	#define MAINCFG_CMD_RMLOGGER "RM_LOGGER"
+	#define MAINCFG_CMD_RMLOGGER_LC "rm_logger"
+	#define MAINCFG_CMD_INITLOGGER "INIT_LOGGER"
+	#define MAINCFG_CMD_INITLOGGER_LC "init_logger"
+	#define MAINCFG_CMD_DEINITLOGGER "DEINIT_LOGGER"
+	#define MAINCFG_CMD_DEINITLOGGER_LC "deinit_logger"
+	#define MAINCFG_CMD_ADDOUTPUTTER "ADD_OUTPUTTER"
+	#define MAINCFG_CMD_ADDOUTPUTTER_LC "add_outputter"
+	#define MAINCFG_CMD_RMOUTPUTTER "RM_OUTPUTTER"
+	#define MAINCFG_CMD_RMOUTPUTTER_LC "rm_outputter"
+	#define MAINCFG_CMD_INITOUTPUTTER "INIT_OUTPUTTER"
+	#define MAINCFG_CMD_INITOUTPUTTER_LC "init_outputter"
+	#define MAINCFG_CMD_DEINITOUTPUTTER "DEINIT_OUTPUTTER"
+	#define MAINCFG_CMD_DEINITOUTPUTTER_LC "deinit_outputter"
+	#define MAINCFG_CMD_LINKLOGGER "LINK_LOGGER"
+	#define MAINCFG_CMD_LINKLOGGER_LC "link_logger"
+	#define MAINCFG_CMD_LINKOUTPUTTER "LINK_OUTPUTTER"
+	#define MAINCFG_CMD_LINKOUTPUTTER_LC "link_outputter"
+
+#elif defined PLATFORM_NIX
+	#define MAINCFG_CMD_EXIT "EXIT"
+	#define MAINCFG_CMD_EXIT_LC "exit"
+	#define MAINCFG_CMD_LS "LS"
+	#define MAINCFG_CMD_LS_LC "ls"
+	#define MAINCFG_CMD_STATE "STATE"
+	#define MAINCFG_CMD_STATE_LC "state"
+	#define MAINCFG_CMD_ADD "ADD"
+	#define MAINCFG_CMD_ADD_LC "add"
+	#define MAINCFG_CMD_RM "RM"
+	#define MAINCFG_CMD_RM_LC "rm"
+	#define MAINCFG_CMD_INIT "INIT"
+	#define MAINCFG_CMD_INIT_LC "init"
+	#define MAINCFG_CMD_DEINIT "DEINIT"
+	#define MAINCFG_CMD_DEINIT_LC "deinit"
+	#define MAINCFG_CMD_PLAY "PLAY"
+	#define MAINCFG_CMD_PLAY_LC "play"
+	#define MAINCFG_CMD_STOP "STOP"
+	#define MAINCFG_CMD_STOP_LC "stop"
+	#define MAINCFG_CMD_ADDLOGGER "ADD_LOGGER"
+	#define MAINCFG_CMD_ADDLOGGER_LC "add_logger"
+	#define MAINCFG_CMD_RMLOGGER "RM_LOGGER"
+	#define MAINCFG_CMD_RMLOGGER_LC "rm_logger"
+	#define MAINCFG_CMD_INITLOGGER "INIT_LOGGER"
+	#define MAINCFG_CMD_INITLOGGER_LC "init_logger"
+	#define MAINCFG_CMD_DEINITLOGGER "DEINIT_LOGGER"
+	#define MAINCFG_CMD_DEINITLOGGER_LC "deinit_logger"
+	#define MAINCFG_CMD_ADDOUTPUTTER "ADD_OUTPUTTER"
+	#define MAINCFG_CMD_ADDOUTPUTTER_LC "add_outputter"
+	#define MAINCFG_CMD_RMOUTPUTTER "RM_OUTPUTTER"
+	#define MAINCFG_CMD_RMOUTPUTTER_LC "rm_outputter"
+	#define MAINCFG_CMD_INITOUTPUTTER "INIT_OUTPUTTER"
+	#define MAINCFG_CMD_INITOUTPUTTER_LC "init_outputter"
+	#define MAINCFG_CMD_DEINITOUTPUTTER "DEINIT_OUTPUTTER"
+	#define MAINCFG_CMD_DEINITOUTPUTTER_LC "deinit_outputter"
+	#define MAINCFG_CMD_LINKLOGGER "LINK_LOGGER"
+	#define MAINCFG_CMD_LINKLOGGER_LC "link_logger"
+	#define MAINCFG_CMD_LINKOUTPUTTER "LINK_OUTPUTTER"
+	#define MAINCFG_CMD_LINKOUTPUTTER_LC "link_outputter"
+#endif
+
+std::map<CFuzzingManager::EWorkerState, std::string> statesNames = {
+	{ CFuzzingManager::EWorkerState::VANILLA, "VANILLA"},
+	{ CFuzzingManager::EWorkerState::INITIALIZED, "INITIALIZED" },
+	{ CFuzzingManager::EWorkerState::PLAYING, "PLAYING" },
+	{ CFuzzingManager::EWorkerState::STOPPED, "STOPPED" },
+	{ CFuzzingManager::EWorkerState::ENDED, "ENDED" },
+	{ CFuzzingManager::EWorkerState::DEINITIALIZED, "DEINITIALIZED" },
+	{ CFuzzingManager::EWorkerState::INTERRUPTED, "INTERRUPTED" }
 };
 
 //Usage: FuzzingFramework cfg_filePath. cfg_filePath implied to be wide string
 #if defined PLATFORM_WINDOWS
-int wmain(int argc, wchar_t** argv) {
+	int wmain(int argc, wchar_t** argv) {
 #elif defined PLATFORM_UNIX
-int main(int argc, char** argv) {
+	int main(int argc, char** argv) {
 #endif
 
 	CFuzzingManager fuzzingManager;
 
 	// Handle user input
-	std::wstring userInputString;
+	MAINCFG_STRTYPE userInputString;
 	while (true) {
-		std::wcout << "> ";
-		std::getline(std::wcin, userInputString);
-		std::wstringstream strStream(userInputString);
-		std::wstring firstWord;
+		std::cout << "> ";//TIP we can mix std::cout and std::wcout usage
+		std::getline(MAINCFG_STD_IN_OBJ, userInputString);
+		STRSTREAM strStream(userInputString);
+		MAINCFG_STRTYPE firstWord;
 		strStream >> firstWord;
-		if ((firstWord == L"EXIT") || (firstWord == L"exit")) {
+		if ((firstWord == MAINCFG_CMD_EXIT) || (firstWord == MAINCFG_CMD_EXIT_LC)) {
 			// Format: "EXIT"
 			break;
 		}
 
-		if ((firstWord == L"LS") || (firstWord == L"ls")) {
+		if ((firstWord == MAINCFG_CMD_LS) || (firstWord == MAINCFG_CMD_LS_LC)) {
 			// Format: "LS"
 			std::vector<std::tuple<TYPE_FUZZERID, CFuzzingManager::EWorkerState, std::vector<TYPE_LOGGERID>, std::vector<TYPE_OUTPUTTERID> >> state;
 			try {
 				state=fuzzingManager.getState();
 			}
-			catch (ExException e) {
-				std::cout << "[ERROR] " << e.msg << std::endl;
+			catch (CFuzzingManager::ExFuzzinManager &e) {
+				std::cout << "[ERROR] " << e.getinfo() << std::endl;
 				continue;
 
 			}
@@ -85,7 +171,7 @@ int main(int argc, char** argv) {
 		}
 
 	//Fuzzer-related commands
-		else if ((firstWord == L"ADD_FUZZER") || (firstWord == L"add_fuzzer")) {
+		else if ((firstWord == MAINCFG_CMD_ADD) || (firstWord == MAINCFG_CMD_ADD_LC)) {
 			//Format: ADD_FUZZER fuzzerID fuzzerLibPath
 			std::wstring fuzzerIDStr;
 			std::wstring fuzzerLibPathStr;
@@ -108,7 +194,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"RM_FUZZER") || (firstWord == L"rm_fuzzer")) {
+		else if ((firstWord == MAINCFG_CMD_RM) || (firstWord == MAINCFG_CMD_RM_LC)) {
 			//Format: RM_FUZZER fuzzerID
 			std::wstring fuzzerIDStr;
 			strStream >> fuzzerIDStr;
@@ -120,7 +206,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"INIT_FUZZER") || (firstWord == L"init_fuzzer")) {
+		else if ((firstWord == MAINCFG_CMD_INIT) || (firstWord == MAINCFG_CMD_INIT_LC)) {
 			//Format: INIT_FUZZER fuzzerID cfgFilePath
 			std::wstring fuzzerIDStr;
 			std::wstring cfgFilePathStr;
@@ -143,7 +229,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"DEINIT_FUZZER") || (firstWord == L"deinit_fuzzer")) {
+		else if ((firstWord == MAINCFG_CMD_DEINIT) || (firstWord == MAINCFG_CMD_DEINIT_LC)) {
 			//Format: DEINIT_FUZZER fuzzerID
 			std::wstring fuzzerIDStr;
 			strStream >> fuzzerIDStr;
@@ -155,7 +241,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"PLAY") || (firstWord == L"play")) {
+		else if ((firstWord == MAINCFG_CMD_PLAY) || (firstWord == MAINCFG_CMD_PLAY_LC)) {
 			//Format: PLAY fuzzerID
 			std::wstring fuzzerIDStr;
 			strStream >> fuzzerIDStr;
@@ -167,7 +253,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"STOP") || (firstWord == L"stop")) {
+		else if ((firstWord == MAINCFG_CMD_STOP) || (firstWord == MAINCFG_CMD_STOP_LC)) {
 			//Format: STOP fuzzerID
 			std::wstring fuzzerIDStr;
 			strStream >> fuzzerIDStr;
@@ -181,7 +267,7 @@ int main(int argc, char** argv) {
 		}
 
 	// Logger-related commands
-		else if ((firstWord == L"ADD_LOGGER") || (firstWord == L"add_logger")) {
+		else if ((firstWord == MAINCFG_ADDLOGGER) || (firstWord == MAINCFG_ADDLOGGER_LC)) {
 			//Format: ADD_LOGGER loggerID loggerLibPath
 			std::wstring loggerIDStr;
 			std::wstring loggerLibPathStr;
@@ -204,7 +290,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"RM_LOGGER") || (firstWord == L"rm_logger")) {
+		else if ((firstWord == MAINCFG_RMLOGGER) || (firstWord == MAINCFG_RMLOGGER_LC)) {
 			//Format: RM_LOGGER loggerID
 			std::wstring loggerIDStr;
 			strStream >> loggerIDStr;
@@ -216,7 +302,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"INIT_LOGGER") || (firstWord == L"init_logger")) {
+		else if ((firstWord == MAINCFG_INITLOGGER) || (firstWord == MAINCFG_INITLOGGER_LC)) {
 			//Format: INIT_LOGGER loggerID cfgFilePath
 			std::wstring loggerIDStr;
 			std::wstring cfgFilePathStr;
@@ -239,7 +325,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"DEINIT_LOGGER") || (firstWord == L"deinit_logger")) {
+		else if ((firstWord == MAINCFG_DEINITLOGGER) || (firstWord == MAINCFG_DEINITLOGGER_LC)) {
 			//Format: DEINIT_LOGGER loggerID
 			std::wstring loggerIDStr;
 			strStream >> loggerIDStr;
@@ -253,7 +339,7 @@ int main(int argc, char** argv) {
 		}
 
 	// Outputter-related commands
-		else if ((firstWord == L"ADD_OUTPUTTER") || (firstWord == L"add_outputter")) {
+		else if ((firstWord == MAINCFG_ADDOUTPUTTER) || (firstWord == MAINCFG_ADDOUTPUTTER_LC)) {
 			//Format: ADD_OUTPUTTER outputterID outputterLibPath
 			std::wstring outputterIDStr;
 			std::wstring outputterLibPathStr;
@@ -276,7 +362,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"RM_OUTPUTTER") || (firstWord == L"rm_outputter")) {
+		else if ((firstWord == MAINCFG_RMOUTPUTTER) || (firstWord == MAINCFG_RMOUTPUTTER_LC)) {
 			//Format: RM_OUTPUTTER outputterID
 			std::wstring outputterIDStr;
 			strStream >> outputterIDStr;
@@ -288,7 +374,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"INIT_OUTPUTTER") || (firstWord == L"init_outputter")) {
+		else if ((firstWord == MAINCFG_INITOUTPUTTER) || (firstWord == MAINCFG_INITOUTPUTTER_LC))) {
 			//Format: INIT_OUTPUTTER outputterID cfgFilePath
 			std::wstring outputterIDStr;
 			std::wstring cfgFilePathStr;
@@ -311,7 +397,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"DEINIT_OUTPUTTER") || (firstWord == L"deinit_outputter")) {
+		else if ((firstWord == MAINCFG_DEINITOUTPUTTER) || (firstWord == MAINCFG_DEINITOUTPUTTER_LC)) {
 			//Format: DEINIT_OUTPUTTER outputterID
 			std::wstring outputterIDStr;
 			strStream >> outputterIDStr;
@@ -325,7 +411,7 @@ int main(int argc, char** argv) {
 		}
 
 	// Links-related commands
-		else if ((firstWord == L"LINK_OUTPUTTER") || (firstWord == L"link_outputter")) {
+		else if ((firstWord == MAINCFG_LINKOUTPUTTER) || (firstWord == MAINCFG_LINKOUTPUTTER_LC)) {
 			//Format: LINK fuzzerID outputterID
 			std::wstring fuzzerIDStr;
 			std::wstring outputterIDStr;
@@ -339,7 +425,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"UNLINK_OUTPUTTER") || (firstWord == L"unlink_outputter")) {
+		else if ((firstWord == MAINCFG_UNLINKOUTPUTTER) || (firstWord == MAINCFG_UNLINKOUTPUTTER_LC)) {
 			//Format: UNLINK fuzzerID outputterID
 			std::wstring fuzzerIDStr;
 			std::wstring outputterIDStr;
@@ -353,7 +439,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"LINK_LOGGER") || (firstWord == L"link_logger")) {
+		else if ((firstWord == MAINCFG_LINKLOGGER) || (firstWord == MAINCFG_LINKLOGGER_LC)) {
 			//Format: LINK fuzzerID outputterID
 			std::wstring fuzzerIDStr;
 			std::wstring loggerIDStr;
@@ -367,7 +453,7 @@ int main(int argc, char** argv) {
 				continue;
 			}
 		}
-		else if ((firstWord == L"UNLINK_LOGGER") || (firstWord == L"unlink_logger")) {
+		else if ((firstWord == MAINCFG_UNLINKLOGGER) || (firstWord == MAINCFG_UNLINKLOGGER_LC)) {
 			//Format: UNLINK fuzzerID outputterID
 			std::wstring fuzzerIDStr;
 			std::wstring loggerIDStr;
